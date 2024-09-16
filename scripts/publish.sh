ffc45367327327070ebd41753c01afd9cb7fb24d
starrnx <starrn.it@gmail.com>
2024-09-16 22:12:55 +0800

    chore: update npm publish scope

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,5 +10,5 @@ cd ../../
 cd packages/plugin-heading-analyzer && npm publish --access public --tag $1
 cd ../../
 
-cd packages/create-puck-app && npm run removeGitignore && npm publish --access public --tag $1 && npm run restoreGitignore
-cd ../../
+# cd packages/create-puck-app && npm run removeGitignore && npm publish --access public --tag $1 && npm run restoreGitignore
+# cd ../../
